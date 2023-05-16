30cb93122176dba61e43c4abc1039ca83f2703d6
EvansSnave <127534196+EvansSnave@users.noreply.github.com>
2023-05-16 09:42:04 -0500

    created menu click function

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const menuItem1 = document.getElementById('item1');
 const menuItem2 = document.getElementById('item2');
 const menuItem3 = document.getElementById('item3');
 const mobileMenu = document.querySelector('.mobile-menu');
+
+hambugerIcon.addEventListener('click', menuClick);
+
+function menuClick() {
+  myLogo.classList.add('visibility-hidden');
+  menuDesktop.classList.add('visibility-hidden');
+  hambugerIcon.classList.add('display-none');
+  closeIcon.classList.add('display-block');
+  menuItem1.classList.add('display-block');
+  menuItem2.classList.add('display-block');
+  menuItem3.classList.add('display-block');
+  mobileMenu.classList.add('mobile-full-height');
+  document.body.classList.add('no-scroll');
+}
