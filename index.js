95c67e02477aa1d90db7f83698b79a0d5fa9c1b9
peteryeungtt@gmail.com <peteryeungtt@gmail.com>
2023-05-16 08:33:57 -0400

    Created html and css codes for mobile menu

---
+++ index.js
@@ -0,0 +1,10 @@
+let myLogo = document.querySelector(".my-logo-wrapper");
+let menuDesktop = document.querySelector(".right-menu")
+let hambugerIcon = document.querySelector(".hamburger-menu");
+let closeIcon = document.getElementById("closeIcon");
+let menuItem1 = document.getElementById("item1");
+let menuItem2 = document.getElementById("item2");
+let menuItem3 = document.getElementById("item3");
+let mobileMenu = document.querySelector(".mobile-menu");
+
+
